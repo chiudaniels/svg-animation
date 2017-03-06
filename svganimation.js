cf3eaa0d9dcdaec023e5f3c8bd314dbb843d1133
chiudaniels <chiudaniels@gmail.com>
2017-03-05 22:41:58 -0500

    done

--- svganimation.js
+++ svganimation.js
@@ -4,6 +4,8 @@ var grow = true;
 var width= svgEle.getAttribute("width");
 var height= svgEle.getAttribute("height")
 var radius = 0;
+var x=Math.round(Math.random() * canv.width);
+var y=Math.round(Math.random() * canv.height);
 
 var clearWindow = function(){
     svgEle.innerHTML="";
@@ -11,36 +13,63 @@ var clearWindow = function(){
 
 var animateCircle= function(){
 
-    // window.cancelAnimationFrame(requestID);
-    // if (radius > 250){
-	// grow=false;
-    // }
-    // else if (radius < 0){
-	// grow=true; 
-    // }
+    window.cancelAnimationFrame(requestID);
+    if (radius > 250){
+	grow=false;
+    }
+    else if (radius < 0){
+	grow=true; 
+    }
 
-    // if (grow){
-	// radius++;
-    // }
-    // else{
-	// radius--;
-    // }
+    if (grow){
+	radius++;
+    }
+    else{
+	radius--;
+    }
 	
-    // clearWindow();
+    clearWindow();
     // console.log(radius);
-	console.log("entered")
+	// console.log("entered")
 	var circle = document.createElementNS("https://www.w3.org/2000/svg", "circle");
     circle.setAttribute("cx", 250);
     circle.setAttribute("cy",250);
-    circle.setAttribute("r", 100);
+    circle.setAttribute("r", radius);
     circle.setAttribute("fill","red");
     svgEle.appendChild(circle);
-	console.log(svgEle.innerHTML)
-    // requestID= window.requestAnimationFrame(animateCircle);
+	// console.log(svgEle.innerHTML)
+    requestID= window.requestAnimationFrame(animateCircle);
 }
 
 var animateDVD = function (){
+	window.cancelAnimationFrame(requestID)
+	xVel=1;
+	yVel=1;
+	var moveDVD = function(){
+		window.cancelAnimationFrame(requestID);
+		clearCanv();
+		var dvd= document.createElementNS("https://www.w3.org/2000/svg", "image");
+		dvd.setAttributeNS("http://www.w3.org/1999/xlink", "href","dvdicon.jpg");
+		dvd.setAttribute({
+			x: x,
+			y: y,
+			height: 75,
+			width: 150;
+		})
+
+		if (x >= canv.width - 150 || x<=0){
+			xVel *= -1;
+		}
+		if (y >= canv.height - 75|| y<=0){
+			yVel *= -1;
+		}
+		x += xVel;
+		y += yVel;
+		requestID = window.requestAnimationFrame(moveDVD);
+		console.log(x,y)
+	}
 	
+	requestID = window.requestAnimationFrame(moveDVD);
 }
 
 var stop = function(){
